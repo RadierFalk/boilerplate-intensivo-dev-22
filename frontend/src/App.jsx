aa67cfb62b1d3700cd68ee5cde4978d92b7f7dca
Rafael Carvalho <94144573+rafaelcarvalhods@users.noreply.github.com>
2022-12-12 20:05:39 -0300

    Install Bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,25 @@ import './App.css'
 function App() {
   return (
     <div className="App">
- 
+      
+      <header>
+        <img scr=""></img>
+        <div>
+          <a>Link1</a>
+          <a>Link2</a>
+          <a>Link3</a>
+        </div>
+      </header>
+
       <div className="content">
+
+
       </div>
 
+      <footer>
+        Todos os direitos reservados ~ 2022.
+      </footer>
+    
     </div>
   )
 }
