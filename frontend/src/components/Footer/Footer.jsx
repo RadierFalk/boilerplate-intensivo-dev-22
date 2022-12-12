f308a6bb8dbead28b5353e604c449796c0fa373a
Diego Medeiros <diego.medeiros@oceanbrasil.com>
2022-12-12 15:49:32 -0400

    Cleaning up

--- frontend/src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-function Footer() {
-  const ano = new Date().getFullYear();
-
-  return <div className="Footer">Todos os direitos reservados ~ {ano}.</div>;
-}
-
-export default Footer;
