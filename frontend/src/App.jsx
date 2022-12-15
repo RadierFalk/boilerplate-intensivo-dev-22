f37d6ad564da5e5db543401482c9a1bbb7f41a2c
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2022-12-15 20:00:28 -0300

    Add Route e Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,17 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Card from "./components/Card/Card"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+        <Route path='/' element={<p>Listagem de itens</p>}></Route>
+        <Route path='/cadastro' element={<p>Cadastro de itens</p>}></Route>
+      </Routes>
 
       <div className="content container mb-3 mt-3">
         <div className="row">
@@ -30,5 +36,6 @@ function App() {
     </div>
   )
 }
+import { Form } from 'react-router-dom'
 
 export default App
