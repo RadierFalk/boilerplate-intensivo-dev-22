566bf25fa7b8d3ffca39ea54829bc59b10728699
Radier Falcao <93263292+RadierFalk@users.noreply.github.com>
2022-12-15 20:52:02 -0300

    estilizando cadastro

--- frontend/src/components/Cadastro/Cadastro.jsx
+++ frontend/src/components/Cadastro/Cadastro.jsx
@@ -1,20 +1,21 @@
 function Cadastro() {
     return(
         <form className="container">
-            <div className="form-group">
+            <h1>Cadastro de Itens</h1>
+            <div className="form-group mt-2">
                 <label>Nome</label>
-                <input name="nome" type="text" />
+                <input className="form-control" name="nome" type="text" />
             </div>
-            <div className="form-group">
+            <div className="form-group mt-2">
                 <label>URL da Imagem:</label>
-                <input name="url" type="url" />
+                <input className="form-control" name="url" type="url" />
             </div>
-            <div className="form-group">
-                <section name="categoria">
+            <div className="form-group mt-2">
+                <section className="form-control" name="categoria">
                     <option>Categoria</option>
                 </section>
             </div>
-            <div className="form-group">
+            <div className="form-group mt-3" >
                 <button type="submit" className="btn bt">Cadastrar</button>
             </div>
         </form>
