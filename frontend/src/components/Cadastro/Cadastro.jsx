475f0c606d031b1cc42bd3255a50ed3de908ca38
RadierFalk <93263292+RadierFalk@users.noreply.github.com>
2022-12-20 20:27:59 -0300

    arrumando cadastro

--- frontend/src/components/Cadastro/Cadastro.jsx
+++ frontend/src/components/Cadastro/Cadastro.jsx
@@ -1,46 +1,76 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import API from "../../api/api";
 
-function Cadastro() {
+function Cadastro(){
     const [categorias, setCategorias] = useState([
-        {_id: 0, name: "Carregando Categorias..."},
+      { _id: 0, name: "Carregando Categorias..." },
     ]);
 
     async function loadCategories() {
         const categoriasDoBanco = await API.category.list()
-        const categoriasCarregadas = await categoriasDoBanco.json(
-        console.log("Carregou!"))
+        const categoriasCarregadas = await categoriasDoBanco.json()
+        console.log("Carregou!!")
         setCategorias(categoriasCarregadas);
-        
-    } 
+    }
 
     useEffect(function(){
         loadCategories();
-    }, []);
-    
+    },[])
 
-    return(
-        <form className="container">
-            <h1>Cadastro de Itens</h1>
-            <div className="form-group mt-2">
-                <label>Nome</label>
-                <input className="form-control" name="nome" type="text" />
-            </div>
-            <div className="form-group mt-2">
-                <label>URL da Imagem:</label>
-                <input className="form-control" name="url" type="url" />
-            </div>
-            <div className="form-group mt-2">
-                <section className="form-control" name="categoria">
-                    <option>Categoria</option>
-                </section>
-            </div>
-            <div className="form-group mt-3" >
-                <button type="submit" className="btn bt">Cadastrar</button>
-            </div>
-        </form>
+    async function cadastroItem(event) {
+        event.preventDefault()
+
+        const formEl = event.target
+
+        formEl.submit.disabled = true
+
+        const bory = {
+            "name" : formEl.nome.value,
+            "image" : formEl.url.value,
+            "category" : formEl.category.value
+        }
+
+        const request = await API.item.create(body)
+
+        formEl.submit.disabled = false
+
+        if (request.status == 201) {
+            alert("Item cadastrado com sucesso!!")
+            formEl.nome.value = ""
+            formEl.url.value = ""
+        }else { 
+            alert("aconteceu algo, seu item NÃO foi cadastrado. :( ")
+        }
+    }
+
+    return (
+      <form className="container" onSubmit={cadastroItem}>  
+        <h1>Cadastro de Items</h1>
+        <div className="form-group mt-2">
+          <label>Nome:</label>
+          <input className="form-control" name="nome" type="text" />
+        </div>
+
+        <div className="form-group mt-2">
+          <label>URL da Imagem:</label>
+          <input className="form-control" name="url" type="url" />
+        </div>
+
+        <div className="form-group mt-2">
+          <label>Categoria:</label>
+          <select className="form-control" name="categoria">
+            {categorias.map(
+                el => <option key={el._id} value ={el._id}>{el.name}</option>
+            )}
+          </select>
+        </div>
+        <div className="form-group mt-3">
+          <button type="submit" className="btn btn-primary">
+            Cadastrar
+          </button>
+        </div>
+      </form>
     );
-};
+}
 
-export default Cadastro;
+export default Cadastro
